72a29288ce7265a00e7dba52e8e4078fbcb22cd7
KhanhIceTea <khanhicetea@gmail.com>
2018-05-24 11:07:09 +0700

    remove unused routes

--- app/Http/Routing.php
+++ app/Http/Routing.php
@@ -2,8 +2,6 @@
 namespace App\Http;
 
 use Pho\Routing\Router;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class Routing extends Router {
     private function to($controller, $method) {
@@ -13,17 +11,5 @@ class Routing extends Router {
     public function routes()
     {
         $this->get('/', $this->to('Home', 'index'), 'home');
-        $this->group('/admin', function($group) {
-            $group->get('/', $this->to('Home', 'abc'), 'abc');
-            $group->get('/xyz', $this->to('Home', 'xyz'), 'xyz');
-        }, [
-            '_before' => function(Request $req) {
-                $req->attributes->set('ok', 'abc');
-            },
-            '_after' => function(Request $req, Response $res) {
-                $res->headers->set('X-Length', $req->attributes->get('ok'));
-                return $res;
-            },
-        ]);
     }
 }
