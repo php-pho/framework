53e68689edcd3258f99010902ad28214e6304b02
Nguyễn Hải Quang <haiquang9994@gmail.com>
2020-04-04 10:02:33 +0700

    cache container

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,13 @@
 <?php
+
 use Pho\Core\Application;
 use Pho\Core\ContainerBuilderFactory;
 
 if (env('APP_ENV', 'dev') == 'dev') {
     $containerBuilder = ContainerBuilderFactory::development();
 } else {
-    $containerBuilder = ContainerBuilderFactory::production(storage_path('cache'), storage_path('cache'));
+    $cacheDir = defined('CONTAINER_CACHE_DIR') ? CONTAINER_CACHE_DIR : 'cache/default';
+    $containerBuilder = ContainerBuilderFactory::production(storage_path($cacheDir), storage_path($cacheDir));
 }
 
 $app = new Application($containerBuilder);
--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
+define('CONTAINER_CACHE_DIR', 'cache/web');
+
 use Pho\Http\HttpProgram;
 
-require dirname(__DIR__).'/bootstrap/load.php';
-require dirname(__DIR__).'/bootstrap/http.php';
+require dirname(__DIR__) . '/bootstrap/load.php';
+require dirname(__DIR__) . '/bootstrap/http.php';
 
 $pho_container = $app->buildContainer();
 $app->run(HttpProgram::class);
